3df85aeddd1665178c9b52de8d5e2d3328414b6f
Ashwani Sharma <sharma.ashwani007@gmail.com>
2013-11-21 23:01:10 +0530

    exercise1 updated

--- exercise1/js/exercise-1.js
+++ exercise1/js/exercise-1.js
@@ -5,13 +5,13 @@ function user (name, age) {
 
 user.prototype.compare = function (user2) {
   if (this.age > user2.age) {
-  	document.write(user1.name + " is older than " + user2.name);
+    document.write(user1.name + " is older than " + user2.name);
   }
   else if (this.age < user2.age) {
     document.write(user2.name + " is older than " + user1.name);
   }
   else {
-  	document.write(user1.name + " is with similar age as " + user2.name);
+    document.write(user1.name + " is with similar age as " + user2.name);
   }
 }
 
