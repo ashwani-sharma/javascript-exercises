30d6f876d77ddfea8094c12435ea920f708e29dc
Ashwani Sharma <sharma.ashwani007@gmail.com>
2013-11-25 21:56:05 +0530

    exercise #2 submitted again

--- exercise2/js/exercise2.js
+++ exercise2/js/exercise2.js
@@ -1,7 +1,10 @@
-function checkAll(value){
-  var childCheck = document.form1.slave;
-  for (var i=0; i<childCheck.length; i++) {
-    childCheck[i].checked = value;
+function getCheckBoxes (thisClass) {
+  this.findCheckBoxes = document.getElementsByClassName(thisClass);
+}
+getCheckBoxes.prototype.selectAllOrNone = function (thisStatus) {
+  for (var i = 0; i < this.findCheckBoxes.length; i++) {
+    this.findCheckBoxes[i].checked = thisStatus;
   }
   return false;
 }
+var allCheckboxes = new getCheckBoxes("slave");
