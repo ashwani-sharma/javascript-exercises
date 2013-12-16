00a48a9141d8d3cb4233eb6dc154ce2f5c176a13
Ashwani Sharma <sharma.ashwani007@gmail.com>
2013-12-16 22:02:13 +0530

    exercise #9 completed

--- exercise9/js/exercise9.js
+++ exercise9/js/exercise9.js
@@ -1,21 +1,8 @@
-function changeItem(valueFrom, valueTo) {
-  var itemFrom = document.form1.elements[valueFrom];
-  var itemTo   = document.form1.elements[valueTo];
-
-  var changeValue = 0;
-  for (var i = 0; i < itemFrom.options.length; i++) {
-    if (itemFrom.options[i].selected) {
-      itemTo.options[itemTo.options.length] = new Option(itemFrom.options[i].text, itemFrom.options[i].value);
-      changeValue++;
-    }
-    else if (changeValue){
-      itemFrom.options[i - changeValue] = new Option(itemFrom.options[i].text, itemFrom.options[i].value);
-    }
-  }
-  if (changeValue){
-    itemFrom.options.length = itemFrom.options.length - changeValue;
-  }
-  else{
-    alert("You haven't selected any options");
+function changeItem(source, destination)
+{
+  var targetSource = document.getElementById(source);
+  var destinationSource = document.getElementById(destination);
+  while (targetSource.selectedIndex != -1) {
+    destinationSource.add(targetSource.options[targetSource.selectedIndex]);
   }
 }
