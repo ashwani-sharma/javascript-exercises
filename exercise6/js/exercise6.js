a8a5b7364a92076670c3ea91ed6c79f720e70ee8
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-06-13 15:38:57 +0530

    exercise #6 form validation completed

--- exercise6/js/exercise6.js
+++ exercise6/js/exercise6.js
@@ -1,38 +1,65 @@
-var register = document.getElementById("userForm");
-var formElements = {
-  textareaAbout : document.getElementById("about-me"),
-  checkbox : document.getElementById("notification"),
-};
-register.addEventListener("submit" ,formvalidation);
-function formvalidation(e) {
-  var flag = false;
-  var element = document.getElementsByClassName("inputvalue");
-  for (var i = 0; i < element.length; i++) {
-    var text = element[i].value.trim(' ');
-    if(text === "") {
-      alert(element[i].id + " cant be left empty");
-      flag = true;
-      break;
+var formValidation = function () {
+  this.init();
+}
+
+formValidation.prototype = {
+  init: function () {
+    this.requiredFields = document.getElementsByClassName("required");
+    this.requiredTextArea = document.getElementById("about");
+    this.requiredCheck = document.getElementById("notification");
+  },
+
+  checkFields: function () {
+    var obj = this,
+        check = true;
+
+    for(i=0; i<obj.requiredFields.length; i++) {
+      if(obj.requiredFields[i].value.trim().length == 0) {
+        alert("Please enter "+ obj.requiredFields[i].name);
+        obj.requiredFields[i].focus();
+        return false;
+      }
     }
-  }
-  if (formElements.textareaAbout.value.trim(' ').length < 50 & flag == false) {
-    alert(" About Me should be minimum 50 characters");
-    flag = true;
-  } 
-  if (notification.checked) {
-    textstring = "want";
-  }
-  else {
-    textstring = "dont want";
-  }
-  if (! flag) {
-    if (confirm("Are you sure you " + textstring + " to receive any notificiaction")) {
+    return check;
+  },
+
+  checkTextAreaCharLimit: function () {
+    var obj = this,
+        check = true;
+        currentCharLength = obj.requiredTextArea.value.trim().length;
+
+    if(currentCharLength < 50) {
+      alert("Please use atleast 50 character to write about yourself");
+      obj.requiredTextArea.focus();
+      check = false;
     }
-    else {
-      flag = true;
+    return check;
+  },
+
+  checkNotificationSelection: function () {
+    var obj = this,
+        check = true;
+    if(!(obj.requiredCheck.checked)) {
+      alert("Please click on checkbox to receive notifications");
+      check = false
     }
+    return check;
+  },
+
+  checkAllValidations: function () {
+    var obj = this,
+        check = false;
+
+    check = obj.checkFields() && obj.checkTextAreaCharLimit() && obj.checkNotificationSelection();
+    return check;
   }
-  if (flag) {
-    e.preventDefault();
+}
+
+window.onload = function () {
+  var validationResult = new formValidation(),
+      form = document.getElementById("user-form");
+
+  form.onsubmit = function () {
+    return validationResult.checkAllValidations();
   }
 }
