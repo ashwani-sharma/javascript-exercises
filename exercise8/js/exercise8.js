bdec0c469035cc53cc57c1127e50c954d4850791
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-06-13 18:03:07 +0530

    exercise #8, input URL and open in new window completed

--- exercise8/js/exercise8.js
+++ exercise8/js/exercise8.js
@@ -2,15 +2,19 @@ function getURL(){
   this.url = "";
   this.enteredURL();
 }
+
 getURL.prototype.enteredURL = function () {
-  this.url = prompt("Please enter a URL you want to open");
-  if (!this.url.trim()) {
+  this.url = prompt("Please enter a URL you want to open", "");
+  if (this.url == null || this.url.length == 0) {
     alert("Can't be empty! Please enter URL again");
     this.enteredURL();
   }
   else {
-    var newWindow = "height=450px,location=0,menubar=0,scrollbars=0,status=0,titlebar=0,toolbar=0,width=400px";
-    window.open(this.url,"",newWindow);
+    var newURL = "http://" + this.url,
+        newWindow = "height=450px, location=0, menubar=0, scrollbars=0, status=0, titlebar=0, toolbar=0, width=400px";
+        
+    window.open(newURL, "", newWindow);
   }  
 }
+
 var newPage = new getURL();
