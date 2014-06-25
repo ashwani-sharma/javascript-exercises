4bc18295650f38a1f9277c4f95c72b96fdf9f3cb
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-06-25 13:52:43 +0530

    exercise #10, email and URL validation added

---
+++ emailUrlValidation-ex10/js/emailUrlValidation-ex10.js
@@ -0,0 +1,38 @@
+var emailRegEx = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]{2,}(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
+    urlRegEx = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+
+var formValidation = function () {
+  this.init();
+}
+
+formValidation.prototype = {
+  init: function () {
+    this.form = document.getElementById("user-form");
+    this.emailField = document.getElementById("email");
+    this.urlField = document.getElementById("homepage");
+  },
+
+  checkValidation: function (input, pattern) {
+    if (!pattern.test(input.value)) {
+      alert("Please enter a valid " + input.name);
+      input.focus();
+      return false;
+    }
+    return true;
+  },
+
+  formValidations: function () {
+    var obj = this;
+    obj.form.onsubmit = function () {
+      if (obj.checkValidation(obj.emailField, emailRegEx) && obj.checkValidation(obj.urlField, urlRegEx)) {
+        return true;
+      }
+      return false;
+    }
+  }
+}
+
+window.onload = function () {
+  var validationResult = new formValidation();
+  validationResult.formValidations();
+}
