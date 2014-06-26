d24770ddbf9311a557113e15094d313578f256cd
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-06-26 18:33:12 +0530

    exercise #12, domain, subdomain matching submitted

---
+++ domainMatching-ex12/js/domainMatching-ex12.js
@@ -0,0 +1,40 @@
+var urlRegEx = /^(?:(?:http|https|ftp):\/\/)?(?:www\.)?(?:((?:[\w\d\_\-]+\.)*[\w\d\-\_]+)\.)?([\w\d\_\-]+\.[\w\d]{2,4})(?:\/[\w\d\-\_\?\=\&\#\.]+)*$/i;
+
+var domainMatch = function () {
+  this.form = document.getElementById("user-form");
+  this.urlField = document.getElementById("url");
+  this.urlField.focus();
+}
+
+domainMatch.prototype = {
+  getValidateUrl: function () {
+    var obj = this;
+    var enteredValue = this.urlField.value.trim();
+
+    return enteredValue.match(urlRegEx);
+  },
+
+  displayDomainInfo: function () {
+    var obj = this;
+
+    obj.form.onsubmit = function () {
+      if(!obj.getValidateUrl()) {
+        alert("Please enter a valid URL");
+        obj.urlField.focus();
+        return false;
+      }
+      
+      if(!RegExp.$1) {
+        alert("Domain: " + RegExp.$2);
+      }
+      else {
+        alert("Domain: " + RegExp.$2 + "\n Sub Domain: " + RegExp.$1);
+      }
+    }
+  }
+}
+
+window.onload = function () {
+  var domainResult = new domainMatch();
+  domainResult.displayDomainInfo();
+}
